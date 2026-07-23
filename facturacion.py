204689e363957309fce5e5be68b3c4741b0fc32e
Julian <julianvasquez027@gmail.com>
2026-07-23 09:32:55 -0600

    Agrega lema de la tienda en la factura

--- facturacion.py
+++ facturacion.py
@@ -31,6 +31,7 @@ def imprimir_factura(cliente, items, frecuente):
     print("=" * 40)
     print(NOMBRE_TIENDA.center(40))
     print("Quetzaltenango, Guatemala".center(40))
+    print("Precios bajos todos los días".center(40))
     print("=" * 40)
     print("Cliente: " + cliente)
     for codigo, cantidad in items:
