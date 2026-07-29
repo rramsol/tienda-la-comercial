34ba94ff0b05433c02e1694da55cacee29105d50
Alejandro Carrillo Garcia <alejandrocarrillog@umes.edu.gt>
2026-07-28 20:56:51 -0600

    personaliza el nombre de la tienda

--- facturacion.py
+++ facturacion.py
@@ -5,7 +5,7 @@
 from inventario import buscar_producto, hay_stock
 from descuentos import descuento_cliente_frecuente
 
-NOMBRE_TIENDA = "La Comercial"
+NOMBRE_TIENDA = "Comercial Carrillo"
 IVA = 0.10
 
 def calcular_subtotal(items):
