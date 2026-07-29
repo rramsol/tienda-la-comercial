60f8571b29c388a672c1c0156b1f78135f14d19a
Javier Sontay <jotasontay27@gmail.com>
2026-07-28 22:26:46 -0600

    personaliza el nombre de la tienda

--- facturacion.py
+++ facturacion.py
@@ -5,7 +5,7 @@
 from inventario import buscar_producto, hay_stock
 from descuentos import descuento_cliente_frecuente
 
-NOMBRE_TIENDA = "La Comercial"
+NOMBRE_TIENDA = "La Bendición"
 IVA = 0.10
 
 def calcular_subtotal(items):
