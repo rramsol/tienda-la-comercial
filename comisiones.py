e577f62130a5eb036515a0bd937bc75677c183da
RicardoCalderon <calderon2005pr@gmail.com>
2026-07-30 09:42:37 -0600

    refactor: estructura

--- comisiones.py
+++ comisiones.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+
 # programa de comisiones
 # hecho por kevin, no tocar, ya funciona
+
 COMISION_BASICA = 0.05
 COMISION_SUPERIOR = 0.08
 
@@ -9,10 +11,9 @@ META_BONO = 50000
 
 BONO = 500
 
-ANCHO_REPORTE = 44 
+ANCHO_REPORTE = 44
 
 
-# lista de vendedores
 vendedores = [
     ("María López", 45000.00),
     ("Carlos Pérez", 28500.00),
@@ -20,38 +21,38 @@ vendedores = [
     ("José Ramírez", 15800.00),
     ("Lucía Morales", 33400.00),
 ]
- 
+
+
+def calcular_total_vendedor(ventas):
+    if ventas > META_COMISION_SUPERIOR:
+        porcentaje_comision = COMISION_SUPERIOR
+    else:
+        porcentaje_comision = COMISION_BASICA
+
+    comision = round(ventas * porcentaje_comision, 2)
+
+    if ventas > META_BONO:
+        bono = BONO
+    else:
+        bono = 0
+
+    return round(comision + bono, 2)
+
+
 def calcular_comisiones():
     total_pagar = 0
+
     print("=" * ANCHO_REPORTE)
     print("    COMISIONES DEL MES - LA COMERCIAL")
     print("=" * ANCHO_REPORTE)
-    # recorre la lista
+
     for vendedor in vendedores:
-        # si vendio mas de 30000
-        if vendedor[1] > META_COMISION_SUPERIOR:
-            # calcula la comision del 8%
-            comision = vendedor[1] * COMISION_SUPERIOR
-            comision = round(comision, 2)
-            # el bono es de 300
-            if vendedor[1] > META_BONO:
-                bono = 500
-            else:
-                bono = 0
-            total_vendedor = round(comision + bono, 2)
-            total_pagar = total_pagar + total_vendedor
-            print(vendedor[0] + ": Q " + str(total_vendedor))
-        else:
-            # calcula la comision del 5%
-            comision = vendedor[1] * COMISION_BASICA
-            comision = round(comision, 2)
-            bono = 0
-            total_vendedor = round(comision + bono, 2)
-            total_pagar = total_pagar + total_vendedor
-            print(vendedor[0] + ": Q " + str(total_vendedor))
-    # ta = tp * 1.12
-    # print("con iva", ta)
+        total_vendedor = calcular_total_vendedor(vendedor[1])
+        total_pagar += total_vendedor
+        print(vendedor[0] + ": Q " + str(total_vendedor))
+
     print("-" * ANCHO_REPORTE)
     print("Total a pagar: Q " + str(round(total_pagar, 2)))
- 
+
+
 calcular_comisiones()
