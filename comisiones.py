33f8eb4377c13ea43c8db6815ffa3b19caeaee7a
Luisdl <quique23@umes.edu.gt>
2026-07-30 09:36:57 -0600

    refactor: estructura

--- comisiones.py
+++ comisiones.py
@@ -17,34 +17,24 @@ vendedores = [
     ("Lucía Morales", 33400.00),
 ]
 
+def calcular_comision(venta):
+    if venta > UMBRAL_VENTA_ALTA:
+        comision = round(venta * TASA_COMISION_ALTA, 2)
+        bono = BONO if venta > UMBRAL_BONO else 0
+    else:
+        comision = round(venta * TASA_COMISION_BAJA, 2)
+        bono = 0
+    return round(comision + bono, 2)
+
 def calcular_comisiones():
     total_pagar = 0
     print("=" * 44)
     print("    COMISIONES DEL MES - LA COMERCIAL")
     print("=" * 44)
-    # recorre la lista
     for nombre, venta in vendedores:
-        # si vendio mas de UMBRAL_VENTA_ALTA
-        if venta > UMBRAL_VENTA_ALTA:
-            # calcula la comision alta
-            comision = round(venta * TASA_COMISION_ALTA, 2)
-            # el bono es de BONO
-            if venta > UMBRAL_BONO:
-                bono = BONO
-            else:
-                bono = 0
-            total_vendedor = round(comision + bono, 2)
-            total_pagar = total_pagar + total_vendedor
-            print(nombre + ": Q " + str(total_vendedor))
-        else:
-            # calcula la comision baja
-            comision = round(venta * TASA_COMISION_BAJA, 2)
-            bono = 0
-            total_vendedor = round(comision + bono, 2)
-            total_pagar = total_pagar + total_vendedor
-            print(nombre + ": Q " + str(total_vendedor))
-    # ta = tp * 1.12
-    # print("con iva", ta)
+        total_vendedor = calcular_comision(venta)
+        total_pagar += total_vendedor
+        print(nombre + ": Q " + str(total_vendedor))
     print("-" * 44)
     print("Total a pagar: Q " + str(round(total_pagar, 2)))
 
