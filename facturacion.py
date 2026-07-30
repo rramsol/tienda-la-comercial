69b2172a24d72dc3c294e9573f108beb40d44984
alexander <alexa@ALEX>
2026-07-29 22:16:23 -0600

    Revert "agrega promocion del mes en la factura"
    This reverts commit 40fe7ec3866f6862e2e64e0cbcfa26b99315b654.

--- facturacion.py
+++ facturacion.py
@@ -44,7 +44,6 @@ def imprimir_factura(cliente, items, frecuente):
     print("Descuento:".ljust(28) + ("Q" + format(descuento, ".2f")).rjust(10))
     print("IVA (12%):".ljust(28) + ("Q" + format(impuesto, ".2f")).rjust(10))
     print("TOTAL:".ljust(28) + ("Q" + format(total, ".2f")).rjust(10))
-    print("*** PROMOCIÓN DEL MES: 2x1 en escobas ***")
     print("Gracias por su compra. Vuelva pronto.")
     print("Quetzaltenango, Guatemala".center(40)) 
     print("Precios bajos todos los días".center(40))
