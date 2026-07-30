0679ece2bfb090196a0299bdad49cef41a10cd54
EmmanuelC-04 <emmanuelchoxom@gmail.com>
2026-07-30 09:32:49 -0600

    refactor: nombres y constantes

--- comisiones.py
+++ comisiones.py
@@ -1,47 +1,74 @@
 # -*- coding: utf-8 -*-
 # programa de comisiones
 # hecho por kevin, no tocar, ya funciona
- 
-# lista de vendedores
-ds = [
+
+ANCHO_REPORTE = 44
+UMBRAL_COMISION_ALTA = 30000
+UMBRAL_BONO = 50000
+TASA_COMISION_ALTA = 0.08
+TASA_COMISION_BASE = 0.05
+BONO_VENTAS_ALTAS = 500
+SIN_BONO = 0
+DECIMALES_MONEDA = 2
+
+VENDEDORES = [
     ("María López", 45000.00),
     ("Carlos Pérez", 28500.00),
     ("Ana García", 61200.00),
     ("José Ramírez", 15800.00),
     ("Lucía Morales", 33400.00),
 ]
- 
-def calc():
-    tp = 0
-    print("=" * 44)
+
+
+def calcular_comisiones():
+    total_a_pagar = 0
+
+    print("=" * ANCHO_REPORTE)
     print("    COMISIONES DEL MES - LA COMERCIAL")
-    print("=" * 44)
+    print("=" * ANCHO_REPORTE)
+
     # recorre la lista
-    for d in ds:
+    for vendedor in VENDEDORES:
         # si vendio mas de 30000
-        if d[1] > 30000:
+        if vendedor[1] > UMBRAL_COMISION_ALTA:
             # calcula la comision del 8%
-            c = d[1] * 0.08
-            c = round(c, 2)
+            comision = vendedor[1] * TASA_COMISION_ALTA
+            comision = round(comision, DECIMALES_MONEDA)
+
             # el bono es de 300
-            if d[1] > 50000:
-                b = 500
+            if vendedor[1] > UMBRAL_BONO:
+                bono = BONO_VENTAS_ALTAS
             else:
-                b = 0
-            tot = round(c + b, 2)
-            tp = tp + tot
-            print(d[0] + ": Q " + str(tot))
+                bono = SIN_BONO
+
+            total_vendedor = round(
+                comision + bono,
+                DECIMALES_MONEDA,
+            )
+            total_a_pagar = total_a_pagar + total_vendedor
+
+            print(vendedor[0] + ": Q " + str(total_vendedor))
         else:
             # calcula la comision del 5%
-            c = d[1] * 0.05
-            c = round(c, 2)
-            b = 0
-            tot = round(c + b, 2)
-            tp = tp + tot
-            print(d[0] + ": Q " + str(tot))
+            comision = vendedor[1] * TASA_COMISION_BASE
+            comision = round(comision, DECIMALES_MONEDA)
+            bono = SIN_BONO
+
+            total_vendedor = round(
+                comision + bono,
+                DECIMALES_MONEDA,
+            )
+            total_a_pagar = total_a_pagar + total_vendedor
+
+            print(vendedor[0] + ": Q " + str(total_vendedor))
+
     # ta = tp * 1.12
     # print("con iva", ta)
-    print("-" * 44)
-    print("Total a pagar: Q " + str(round(tp, 2)))
- 
-calc()
+    print("-" * ANCHO_REPORTE)
+    print(
+        "Total a pagar: Q "
+        + str(round(total_a_pagar, DECIMALES_MONEDA))
+    )
+
+
+calcular_comisiones()
